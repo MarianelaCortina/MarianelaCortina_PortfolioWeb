f03c64522d894c79168218ff4d78359e6393831b
LautaroLandolfo <marianelacort@hotmail.com>
2021-03-10 16:11:48 -0300

    mi primer commit

---
+++ Controller/consulta.php
@@ -0,0 +1,38 @@
+<?PHP
+
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' .$email . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $email . "\r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . "\r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'marianelacort@hotmail.com';
+$asunto = 'Mensaje de mi Portfolio Web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location: home.html");
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
